84996f7c3a4875c6cb067939cd9f2c0ee8ec528c
Vinicius <hutlun2014@gmail.com>
2020-06-26 11:13:12 -0300

    Rascunho da página de perfil

--- Ainidiu/lib/src/page/perfil_page.dart
+++ Ainidiu/lib/src/page/perfil_page.dart
@@ -11,7 +11,16 @@ class _PerfilPageState extends State<PerfilPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-    );
+          child: Column(
+            children: <Widget>[
+              Row(mainAxisAlignment: MainAxisAlignment.end,children: <Widget>[Padding(padding: EdgeInsets.only(top:5.0, right: 5.0),child: Icon(Icons.settings, size: 40.0))]),
+              CircleAvatar(minRadius: 40.0, child: Icon(Icons.person, size: 50.0)),
+              Padding(padding: EdgeInsets.only(top:10.0),child: Container(width: 200,decoration: BoxDecoration(border: Border.all(color: Colors.black)),child: Center(child:Text('Apelido', style: TextStyle(fontWeight: FontWeight.bold)) ))),
+              Padding(padding: EdgeInsets.only(top:10.0),child: Container(width: 200,decoration: BoxDecoration(border: Border.all(color: Colors.black)),child: Center(child:Text(a, style: TextStyle(fontWeight: FontWeight.bold)) )))
+              
+             
+            ]
+          )
+        );
   }
 }
